5f0462e4f6607a6ad32b6a2f3eb8c762f375dea3
Szymon Borucki <szymon.borucki@gmail.com>
2016-07-04 17:26:52 +0200

    add message broadcast

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  mount ActionCable.server => '/cable'
+
   resources :chat_rooms, only: [:new, :create, :show, :index]
   root 'chat_rooms#index'
 end
